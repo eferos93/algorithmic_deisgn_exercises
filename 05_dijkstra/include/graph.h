1e161af6cf7582feaa79b05f85e5459572ccf903
Eros Fabrici <eros.fabrici@gmail.com>
2020-06-07 17:51:02 +0200

    Implementation done

--- 05_dijkstra/include/graph.h
+++ 05_dijkstra/include/graph.h
@@ -8,7 +8,7 @@
 
 typedef struct Graph
 {
-    int num_nodes;
+    size_t num_nodes;
     //unsigned int* weights_matrix; //adj matrix
     node_type* nodes;
 } graph_type;
@@ -19,7 +19,7 @@ typedef struct Node
     int key;
     //size_t key_size;
     linked_list* adjacents;
-    int distance_from_source;
+    size_t distance_from_source;
     //unsigned int position; //position inside the heap
     node_type* predecessor;
 } node_type;
@@ -32,10 +32,13 @@ typedef struct Adjacents
 */
 int order_node(const void * node_a, const void* node_b);
 
-node_type create_node(int key);
-node_type *create_nodes(unsigned int num_of_nodes);
-graph_type* create_graph(unsigned int num_of_nodes);
+node_type create_node(int key, linked_list* adj_list);
+node_type *create_nodes(size_t num_of_nodes, linked_list* adj_lists);
+graph_type* create_graph(size_t num_of_nodes, linked_list* adj_lists);
+void dijkstra_minheap(graph_type* g, node_type* source);
+void dijkstra_array(graph_type* g, node_type* source);
+void delete_graph(graph_type* g);
+void delete_node(node_type* node);
 
-void dijkstra(graph_type* g, node_type* source);
 
 #endif
--- 05_dijkstra/src/graph.c
+++ 05_dijkstra/src/graph.c
@@ -1,15 +1,13 @@
 #include <graph.h>
 
 
-node_type *create_nodes(unsigned int num_of_nodes)
+node_type *create_nodes(size_t num_of_nodes, linked_list* adj_lists)
 {
     node_type *new_nodes = (node_type *)malloc(sizeof(node_type)*num_of_nodes);
     for (size_t i = 0; i < num_of_nodes; i++)
     {
-        //new_nodes[i].key = i;
-        //new_nodes[i].distance_from_source = UINT_MAX;
-        //new_nodes[i].predecessor = NULL;
-        new_nodes[i] = create_node(i);
+        //new_nodes[i] = create_node(i, adj_lists+(i*sizeof(linked_list)));
+        new_nodes[i] = create_node(i, NULL);
     }
     
     //new_node->key = key;
@@ -20,10 +18,10 @@ node_type *create_nodes(unsigned int num_of_nodes)
     return new_nodes;
 }
 
-node_type create_node(int key)
+node_type create_node(int key, linked_list* adj_list)
 {
     node_type new_node;// = (node_type*) malloc(sizeof(node_type));
-    new_node.adjacents = NULL;
+    new_node.adjacents = adj_list;
     new_node.key = key;
     new_node.distance_from_source = UINT_MAX;
     new_node.predecessor = NULL;
@@ -73,12 +71,12 @@ adjacents_type adjacents(graph_type *g, node_type *node, int n)
     return adj;
 }
 */
-graph_type *create_graph(unsigned int num_nodes)
+graph_type *create_graph(size_t num_nodes, linked_list* adj_lists)
 {
     graph_type *graph = (graph_type *)malloc(sizeof(graph_type));
     graph->num_nodes = num_nodes;
     //graph->weights_matrix = adj_matrix;
-    graph->nodes = create_nodes(num_nodes);
+    graph->nodes = create_nodes(num_nodes, adj_lists);
     return graph;
 }
 
@@ -94,9 +92,15 @@ void initialise(graph_type *g)
 
 void update_distance(binheap_type *queue, node_type *node, unsigned int new_distance)
 {
-    decrease_key(queue, node->position, new_distance);
+    decrease_key(queue, (void*) node, new_distance);
 }
 
+/**
+ * @brief Relax for min-heap implementation
+ * @param u Starting node
+ * @param v Node which distance is to be updated
+ * @param weight Weight between u and v
+ */
 void relax(binheap_type *queue, node_type *u, node_type *v, unsigned int weight)
 {
     if (u->distance_from_source + weight < v->distance_from_source)
@@ -106,7 +110,13 @@ void relax(binheap_type *queue, node_type *u, node_type *v, unsigned int weight)
     }
 }
 
-void relax(node_type *u, node_type *v, unsigned int weight)
+/**
+ * @brief Relax for vector implementation
+ * @param u Starting node
+ * @param v Node which distance is to be updated
+ * @param weight Weight between u and v
+ */
+void relax_array(node_type *u, node_type *v, size_t weight)
 {
     if (u->distance_from_source + weight < v->distance_from_source)
     {
@@ -114,7 +124,7 @@ void relax(node_type *u, node_type *v, unsigned int weight)
         v->predecessor = u;
     }
 }
-
+/*
 node_type* getNode(graph_type* g, int key)
 {
     for(int i = 0; i < g->num_nodes; i++)
@@ -126,28 +136,101 @@ node_type* getNode(graph_type* g, int key)
     }
     return NULL;
 }
-
+*/
 
 int order_node(const void* node_a, const void* node_b)
 {
-    return leq_int((void *) &(((node_type*) node_a)->key), (void *) &(((node_type*) node_b)->key));
+    return leq_int((void *) &(((node_type*) node_a)->distance_from_source), (void *) &(((node_type*) node_b)->distance_from_source));
 }
 
-int leq_int(const void *a, const void *b)
-{
-  return *((int*)a)<=*((int*)b);
-}
 
-void dijkstra(graph_type* g, node_type* source)
+void dijkstra_minheap(graph_type* g, node_type* source)
 {
     initialise(g);
     source->distance_from_source = 0;
-    binheap_type* queue = build_heap((void*) g->nodes, g->num_nodes, g->num_nodes-1, sizeof(int), order_node);
+    binheap_type* queue = build_heap((void*) g->nodes, g->num_nodes, g->num_nodes-1, sizeof(unsigned int), order_node);
 
     while (!is_heap_empty(queue))
     {
         node_type* u = extract_min(queue);
-        //adjacents(g, u, )
+        list_node* current = u->adjacents->head;
+        while (current)
+        {
+            relax(queue, u, current->element, current->weight);
+            current = current->next;
+        }
+    }
+    delete_heap(queue);
+}
+
+node_type* extract_min_array(linked_list* nodes)
+{    
+    list_node* min = nodes->head;
+    list_node* before_min = NULL;
+    list_node* current = min->next;
+    list_node* before_current = min;
+    node_type* curr_node;
+    node_type* min_node = (node_type*) min->element;
+
+    while (current)
+    {
+        curr_node = (node_type*) current->element;
+        if (curr_node->distance_from_source < min_node->distance_from_source)
+        {
+            min = current;
+            min_node = (node_type*) min->element;
+            before_min = before_current;
+        }
+        before_current = current;
+        current = current->next;
     }
     
+    before_min->next = min->next;
+    free(min);
+    nodes->length--;
+    return min_node;
+}
+
+linked_list* build_array(void* elements, size_t num_of_elem, size_t elem_size)
+{
+    linked_list* list = create_linked_list(elements, elem_size, 0);
+    for (size_t i = 1; i < num_of_elem; i++)
+    {
+        insert_new_node(list, elements+(i*elem_size), 0);
+    }
+    return list;
+}
+
+void dijkstra_array(graph_type* g, node_type* source)
+{
+    initialise(g);
+    source->distance_from_source = 0;
+    
+    linked_list* queue = build_array((void *) g->nodes, g->num_nodes, sizeof(node_type));
+
+    while (queue->length != 0)
+    {
+        node_type* u = extract_min_array(queue);
+        list_node* current = u->adjacents->head;
+        while (current)
+        {
+            relax_array(u, current->element, current->weight);
+            current = current->next;
+        }
+    }
+    free(queue);
+}
+
+void delete_node(node_type* node)
+{
+    free(node->adjacents);
+    free(node);
+}
+void delete_graph(graph_type* g)
+{
+    for (size_t i = 0; i < g->nodes; i++)
+    {
+        delete_node(g->nodes + i*sizeof(node_type));
+    }
+    free(g);
 }
